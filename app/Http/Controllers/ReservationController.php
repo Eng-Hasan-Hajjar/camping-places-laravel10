09c1b54a64341a5fe34600a5ded656c0ea36574c
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-04-28 07:19:24 -0700

    add new functions to reservation controller

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,8 +3,85 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reservation;
 
 class ReservationController extends Controller
 {
-    //
+
+
+
+     // عرض النموذج لإنشاء حجز جديد
+     public function create()
+     {
+         return view('reservations.create');
+     }
+
+     // حفظ الحجز الجديد في قاعدة البيانات
+     public function store(Request $request)
+     {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'camp_ground_id' => 'required|exists:camp_grounds,id',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ]);
+
+
+         Reservation::create([
+             'user_id' => $request->user_id,
+             'camp_ground_id' => $request->camp_ground_id,
+             'start_date' => $request->start_date,
+             'end_date' => $request->end_date,
+         ]);
+
+         return redirect()->route('reservations.index')->with('success', 'تم إنشاء الحجز بنجاح.');
+     }
+
+     // عرض التفاصيل لحجز معين
+     public function show(Reservation $reservation)
+     {
+         return view('reservations.show', compact('reservation'));
+     }
+       // الوظيفة لعرض جميع الحجوزات
+    public function showAll()
+    {
+        $reservations = Reservation::all();
+        return view('reservations.all', compact('reservations'));
+
+    }
+
+     // عرض النموذج لتعديل حجز معين
+     public function edit(Reservation $reservation)
+     {
+         return view('reservations.edit', compact('reservation'));
+     }
+
+     // تحديث بيانات الحجز في قاعدة البيانات
+     public function update(Request $request, Reservation $reservation)
+     {
+
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'camp_ground_id' => 'required|exists:camp_grounds,id',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ]);
+
+         $reservation->update([
+             'user_id' => $request->user_id,
+             'camp_ground_id' => $request->camp_ground_id,
+             'start_date' => $request->start_date,
+             'end_date' => $request->end_date,
+         ]);
+
+         return redirect()->route('reservations.index')->with('success', 'تم تحديث الحجز بنجاح.');
+     }
+
+     // حذف حجز معين من قاعدة البيانات
+     public function destroy(Reservation $reservation)
+     {
+         $reservation->delete();
+         return redirect()->route('reservations.index')->with('success', 'تم حذف الحجز بنجاح.');
+     }
+
 }
