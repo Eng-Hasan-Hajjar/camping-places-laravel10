9cb6c99de52dfb4cfb9a2eccc375a4b82daee164
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-04-28 05:26:45 -0700

    new model for reservation and new fields

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['user_id', 'camp_ground_id', 'start_date', 'end_date'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function campGround()
+    {
+        return $this->belongsTo(CampGround::class);
+    }
 }
