6abf2a6dff2c7e76695acff583bc27f662358b59
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-05-09 11:26:30 -0700

    add new routes for guides

--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -99,6 +99,34 @@
 
 
 
+{{-- Guides --}}
+
+
+<li class="nav-item has-treeview">
+    <a href="#" class="nav-link">
+      <i class="nav-icon fas fa-users"></i>
+      <p>
+        الأدلة السياحيين
+        <i class="fas fa-angle-left right"></i>
+      </p>
+    </a>
+    <ul class="nav nav-treeview">
+      <li class="nav-item">
+        <a href="{{url('/adminpanel/guides/create')}}" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>إضافة دليل سياحي </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="{{url('/adminpanel/guides/')}}" class="nav-link">
+          <i class="far fa-circle nav-icon"></i>
+          <p>كل الأدلة السياحيين</p>
+        </a>
+      </li>
+
+    </ul>
+  </li>
+
 {{-- Doctors --}}
 
 
