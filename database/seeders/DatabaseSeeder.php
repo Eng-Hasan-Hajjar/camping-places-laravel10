00f468e4d5e05ed2618dbc51591d8ddec540b6ff
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-06-12 16:46:08 -0700

    new instructions to  views for camp ground doctors guid

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,16 +22,104 @@ class DatabaseSeeder extends Seeder
           'name' => 'Test User',
           'email' => 'test@example.com',
           'password'=>'123456789',
+          'role'=>'admin',
         ]);
         \App\Models\User::create([
             'name' => 'Test User 2',
             'email' => 'test2@example.com',
             'password'=>'123456789',
+            'role'=>'visitor',
           ]);
           \App\Models\User::create([
             'name' => 'Test User 3',
             'email' => 'test3@example.com',
             'password'=>'123456789',
           ]);
+
+          \App\Models\Doctor::create([
+            'name' => 'Test Doctor ',
+            'phon' => '0933912076',
+            'is_free'=>'0',
+            'specialty'=>'قلبية',
+          ]);
+          \App\Models\Doctor::create([
+            'name' => 'Test Doctor 2',
+            'phon' => '0933912076',
+            'is_free'=>'1',
+            'specialty'=>'داخلية',
+          ]);
+
+          \App\Models\Doctor::create([
+            'name' => 'Test Doctor 3',
+            'phon' => '0933912076',
+            'is_free'=>'1',
+            'specialty'=>'جراحة عامة',
+          ]);
+          \App\Models\Doctor::create([
+            'name' => 'Test Doctor 4',
+            'phon' => '0933912076',
+            'is_free'=>'1',
+            'specialty'=>'داخلية',
+          ]);
+          \App\Models\Guide::create([
+            'name' => 'Test Guid 1',
+            'phon' => '0933912076',
+            'is_free'=>'1',
+
+          ]);
+          \App\Models\Guide::create([
+            'name' => 'Test Guid 2',
+            'phon' => '0933912076',
+            'is_free'=>'1',
+
+          ]);
+          \App\Models\Guide::create([
+            'name' => 'Test Guid 3',
+            'phon' => '0933912076',
+            'is_free'=>'1',
+
+          ]);
+
+          \App\Models\CampGround::create([
+            'name' => 'بادية الشام',
+            'description' => 'من أجمل الأماكن ',
+            'country'=>'سوريا',
+            'city' => 'تدمر',
+            'region' => 'البادية',
+            'cm_type'=>'1',
+            'cm_season' => '',
+            'campGround_image' => '',
+            'google_image'=>'',
+            'forecast' => 'غائم جزئي',
+
+          ]);
+          \App\Models\CampGround::create([
+            'name' => 'بادية سيناء',
+            'description' => 'من أجمل الأماكن ',
+            'country'=>'مصر',
+            'city' => 'سيناء',
+            'region' => 'العريش',
+            'cm_type'=>'1',
+            'cm_season' => '',
+            'campGround_image' => '',
+            'google_image'=>'',
+            'forecast' => 'غائم جزئي',
+
+          ]);
+          \App\Models\CampGround::create([
+            'name' => 'غابات الأمازون',
+            'description' => 'من أجمل الأماكن ',
+            'country'=>'البرازيل',
+            'city' => 'سامباولو',
+            'region' => 'سامبو',
+            'cm_type'=>'2',
+            'cm_season' => '',
+            'campGround_image' => '',
+            'google_image'=>'',
+            'forecast' => 'غائم جزئي',
+
+          ]);
+
+
     }
 }
